bf17ed2fcbf3492ac522857f561b31245279e782
bryunx <bjlfrey@gmail.com>
2019-08-06 23:49:20 -0700

    StyledHero.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,7 @@ import Banner from '../components/Banner';
 import { Link } from 'react-router-dom';
 import Services from '../components/Services';
 import FeaturedRooms from '../components/FeaturedRooms';
+import Button from '../components/StyledHero';
 
 export default function Home() {
     return (
@@ -16,6 +17,7 @@ export default function Home() {
         </Hero>
         <Services />
         <FeaturedRooms />
+        <Button>hello</Button>
     </>
     );
 }
--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -7,15 +7,35 @@ import {RoomContext} from '../context';
 
 export default class SingleRoom extends Component {
     constructor (props){
-        super(props)
-
+        super(props);
+        //console.log(this.props);
+        this.state = {
+            slug: this.props.match.params.slug,defaultBcg
+        };
     }
+    static contextType = RoomContext;
+
+    // componentDidMount() {}
     render() {
-        return (
-            <div>hello from single room page
+        const { getRoom } = this.context;
+        const room = getRoom(this.state.slug);
+        console.log(room);
+        if(!room){
+            return <div className = "error">
+                <h3>no such room could be found...</h3>
+                <Link to = "/rooms" className = "btn-primary">
+                    back to rooms
+                </Link>
             </div>
-        )
+            
+        }
+        const {name, description,capacity,size,price,extras,breakfast,pets,images} = room
+        return <Hero hero='roomsHero'>
+            <Banner title={`${name} room`}>
+                <Link to='/rooms' className='btn-primary'>
+                    back to rooms
+                </Link>
+            </Banner>
+        </Hero>
     }
 }
-
-//2:32:08
