dba0ae97b2023efba9ba884f154759ea083cae34
bryunx <bjlfrey@gmail.com>
2019-08-11 21:59:33 -0700

    update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,7 +10,7 @@ export default function Home() {
     <>
         <Hero>
             <Banner title="luxurious rooms" subtitle="deluxe rooms starting at $299">
-                <Link to="/rooms" className="btn-primary">NOT OUR ROOMS UPDATE 2
+                <Link to="/rooms" className="btn-primary">OUR ROOMS
                 </Link>
             </Banner>
         </Hero>
